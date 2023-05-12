fd4aea9bfbe05d7ea17cf7b6e465990a2363c115
andreeatalpos <andreeatalpos2000@yahoo.com>
2023-05-13 00:33:19 +0300

    image list working

--- components/AnimalForm.js
+++ components/AnimalForm.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import * as Yup from "yup";
 import {
   ButtonText,
@@ -10,6 +10,7 @@ import {
   StyledTextInputForm,
   StyledTextInputFormBigger,
   SubTitle,
+  StyledErrorText,
 } from "./styles";
 import { Alert } from "react-native";
 import KeyboardAvoidingWrapper from "./KeyboardAvoidingWrapper";
@@ -21,7 +22,7 @@ import { useRoute } from "@react-navigation/native";
 import { Formik } from "formik";
 
 // validation schema
-const AnimalSchema = Yup.object().shape({
+const animalSchema = Yup.object().shape({
   animalName: Yup.string().required("Animal name is required"),
   breed: Yup.string().required("Breed is required"),
   color: Yup.string().required("Color is required"),
@@ -39,27 +40,87 @@ const AnimalForm = ({ route }) => {
   console.log("file  " + imageFileName);
   const { storedCredentials, setStoredCredentials } =
     useContext(CredentialsContext);
+  const [animalData, setAnimalData] = useState({
+    animalName: "",
+    animalInfo: "",
+    breed: "",
+    color: "",
+    age: "",
+    species: "",
+    isFound: isFound,
+    username: storedCredentials.username,
+    fileName: imageFileName,
+  });
+  const [animalName, setAnimalName] = useState("");
+  const [animalInfo, setAnimalInfo] = useState("");
+  const [breed, setBreed] = useState("");
+  const [color, setColor] = useState("");
+  const [age, setAge] = useState("");
+  const [species, setSpecies] = useState("");
+  const [validationErrors, setValidationErrors] = useState({});
+  const [errors, setErrors] = useState({});
+  const navigation = useNavigation();
 
-  async function handleFormSubmit(values) {
-    const animalData = {
-      animalName: values.animalName,
-      animalInfo: values.animalInfo,
-      breed: values.breed,
-      color: values.color,
-      age: parseInt(values.age),
-      species: values.species,
+  //   async function handleFormSubmit(values) {
+  //     const animalData = {
+  //       animalName: values.animalName,
+  //       animalInfo: values.animalInfo,
+  //       breed: values.breed,
+  //       color: values.color,
+  //       age: parseInt(values.age),
+  //       species: values.species,
+  //       isFound: isFound,
+  //       username: storedCredentials.username,
+  //       fileName: imageFileName,
+  //     };
+  //     try {
+  //       console.log(animalData);
+  //       const response = await baseAxios.post("animal", animalData);
+  //       console.log(response);
+  //     } catch (error) {
+  //       console.log("eroare la create animal");
+  //       console.error(error.message);
+  //     }
+  //   }
+
+  async function handleFormSubmit(event) {
+    event.preventDefault();
+    const data = {
+      animalName: animalName,
+      animalInfo: animalInfo,
+      breed: breed,
+      color: color,
+      age: parseInt(age),
+      species: species,
       isFound: isFound,
       username: storedCredentials.username,
       fileName: imageFileName,
     };
-    try {
-      console.log(animalData);
-      const response = await baseAxios.post("animal", animalData);
-      console.log(response);
-    } catch (error) {
-      console.log("eroare la create animal");
-      console.error(error.message);
-    }
+    animalSchema
+      .validate(data, { abortEarly: false })
+      .then(async () => {
+        try {
+          console.log(animalData);
+          const response = await baseAxios.post("animal", data);
+          console.log(response);
+          Alert.alert("Success", "Animal info submitted successfully", [
+            {
+              text: "OK",
+              onPress: () => navigation.pop(),
+            },
+          ]);
+        } catch (error) {
+          console.log("eroare la create animal");
+          console.error(error.message);
+        }
+      })
+      .catch((error) => {
+        const errors = {};
+        error.inner.forEach((e) => {
+          errors[e.path] = e.message;
+        });
+        setErrors(errors);
+      });
   }
 
   return (
--- components/styles.js
+++ components/styles.js
@@ -1,158 +1,186 @@
-import styled from 'styled-components';
-import {View, Text, Image, TextInput, TouchableOpacity} from 'react-native';
-import Constants from 'expo-constants';
+import styled from "styled-components";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import Constants from "expo-constants";
 
 const StatusBarHeight = Constants.statusBarHeight;
 
 //colors
 
 export const Colors = {
-    white: "#FFFFFF",
-    black: "#000000",
-    pink: "#FFD1DC",
-    orange: "#FFA07A",
-    light_yellow: "#FFF8DC",
-    blue: "#ADD8E6",
-    purple: "#B19CD9",
-    grey: "#C9C9C9",
-    green: "#B4E197",
-    red: "#FF6961",
-    light_green: 'rgba(16, 185, 129, 0.1)'
-}
+  white: "#FFFFFF",
+  black: "#000000",
+  pink: "#FFD1DC",
+  orange: "#FFA07A",
+  light_yellow: "#FFF8DC",
+  blue: "#ADD8E6",
+  purple: "#B19CD9",
+  grey: "#C9C9C9",
+  green: "#B4E197",
+  red: "#FF6961",
+  light_green: "rgba(16, 185, 129, 0.1)",
+};
 
-
-const {white, pink, orange, light_yellow, blue, purple, grey, green, red, light_green, black} = Colors
+const {
+  white,
+  pink,
+  orange,
+  light_yellow,
+  blue,
+  purple,
+  grey,
+  green,
+  red,
+  light_green,
+  black,
+} = Colors;
 
 export const StyledContainer = styled.View`
-    flex: 1;
-    padding: 25px;
-    padding-top: ${StatusBarHeight +30}px;
-    padding-bottom: 45%;
-    background-color: ${white};
-    flexGrow: 1;
+  flex: 1;
+  padding: 25px;
+  padding-top: ${StatusBarHeight + 30}px;
+  padding-bottom: 45%;
+  background-color: ${white};
+  flexgrow: 1;
 `;
 
 export const StyledContainerAnimal = styled.View`
-    flex: 1;
-    padding: 25px;
-    padding-top: 5%;
-    background-color: ${white};
-    flexGrow: 1;
+  flex: 1;
+  padding: 25px;
+  padding-top: 5%;
+  background-color: ${white};
+  flexgrow: 1;
 `;
 
 export const StyledTextInputForm = styled.TextInput`
-    background-color: ${light_yellow};
-    padding: 15px;
-    padding-left: 20px;
-    padding-right: 55px;
-    border-radius: 5px;
-    font-size: 15px;
-    height: 60px;
-    margin-vertical: 3px;
-    margin-bottom: 10px;
-    color: ${black};
+  background-color: ${light_yellow};
+  padding: 15px;
+  padding-left: 20px;
+  padding-right: 55px;
+  border-radius: 5px;
+  font-size: 15px;
+  height: 60px;
+  margin-vertical: 3px;
+  margin-bottom: 10px;
+  color: ${black};
+`;
+
+export const StyledErrorText = styled.TextInput`
+  background-color: ${light_yellow};
+  padding: 15px;
+  padding-left: 20px;
+  padding-right: 55px;
+  border-radius: 5px;
+  font-size: 15px;
+  height: 60px;
+  margin-vertical: 3px;
+  margin-bottom: 10px;
+  color: ${red};
 `;
 
 export const StyledTextInputFormBigger = styled.TextInput`
-    background-color: ${light_yellow};
-    padding: 15px;
-    padding-left: 20px;
-    padding-right: 55px;
-    border-radius: 5px;
-    font-size: 15px;
-    height: 100px;
-    margin-vertical: 3px;
-    margin-bottom: 10px;
-    color: ${black};
+  background-color: ${light_yellow};
+  padding: 15px;
+  padding-left: 20px;
+  padding-right: 55px;
+  border-radius: 5px;
+  font-size: 15px;
+  height: 100px;
+  margin-vertical: 3px;
+  margin-bottom: 10px;
+  color: ${black};
 `;
 
-
 export const InnerContainer = styled.View`
-    flex: 1;
-    width: 100%;
-    align-items: center;
+  flex: 1;
+  width: 100%;
+  align-items: center;
 `;
 
 export const PageLogo = styled.Image`
-    width: 200px;
-    height: 175px;
+  width: 200px;
+  height: 175px;
 `;
 
 export const PageTitle = styled.Text`
-    font-size: 30px;
-    text-align: center;
-    font-weight: bold;
-    color: ${purple};
-    padding: 15px;
+  font-size: 30px;
+  text-align: center;
+  font-weight: bold;
+  color: ${purple};
+  padding: 15px;
 
-    ${(props) => props.home == true && `
+  ${(props) =>
+    props.home == true &&
+    `
         font-size: 35px;
     `}
-
 `;
 
 export const SubTitle = styled.Text`
-    font-size: 18px;
-    margin-bottom: 20px;
-    letter-spacing: 1.5px;
-    font-weight: bold;
-    color: ${blue};
-    text-align: center;
+  font-size: 18px;
+  margin-bottom: 20px;
+  letter-spacing: 1.5px;
+  font-weight: bold;
+  color: ${blue};
+  text-align: center;
 
-    ${(props) => props.home == true && `
+  ${(props) =>
+    props.home == true &&
+    `
         margin-bottom: 5px;
         font-weight: normal;
     `}
 `;
 
 export const StyledFormArea = styled.View`
-    width: 80%;
-    margin-left: 10%;
+  width: 80%;
+  margin-left: 10%;
 `;
 
 export const StyledTextInput = styled.TextInput`
-    background-color: ${light_yellow};
-    padding: 15px;
-    padding-left: 55px;
-    padding-right: 55px;
-    border-radius: 5px;
-    font-size: 15px;
-    height: 60px;
-    margin-vertical: 3px;
-    margin-bottom: 10px;
-    color: ${purple};
+  background-color: ${light_yellow};
+  padding: 15px;
+  padding-left: 55px;
+  padding-right: 55px;
+  border-radius: 5px;
+  font-size: 15px;
+  height: 60px;
+  margin-vertical: 3px;
+  margin-bottom: 10px;
+  color: ${purple};
 `;
 
 export const StyledInputLabel = styled.Text`
-    color: ${purple};
-    font-size: 13px;
-    text-align: left;
+  color: ${purple};
+  font-size: 13px;
+  text-align: left;
 `;
 
 export const LeftIcon = styled.View`
-    left: 15px;
-    top: 38%;
-    position: absolute;
-    z-index: 1;
+  left: 15px;
+  top: 38%;
+  position: absolute;
+  z-index: 1;
 `;
 
 export const RightIcon = styled.TouchableOpacity`
-    right: 15px;
-    top: 38%;
-    position: absolute;
-    z-index: 1;
+  right: 15px;
+  top: 38%;
+  position: absolute;
+  z-index: 1;
 `;
 
 export const StyledButton = styled.TouchableOpacity`
-    padding: 15px;
-    background-color: ${pink};
-    justify-content: center;
-    align-items: center;
-    border-radius: 5px;
-    margin-vertical: 5px;
-    height: 60px;
+  padding: 15px;
+  background-color: ${pink};
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  margin-vertical: 5px;
+  height: 60px;
 
-    // ${(props) => props.google == true && `
+  // ${(props) =>
+    props.google == true &&
+    `
     //     background-color: ${green};
     //     flex-direction: row;
     //     justify-content: center;
@@ -160,122 +188,124 @@ export const StyledButton = styled.TouchableOpacity`
 `;
 
 export const ButtonText = styled.Text`
-    color: ${purple};
-    font-size: 16px;
+  color: ${purple};
+  font-size: 16px;
 
-    ${(props) => props.google == true && `
+  ${(props) =>
+    props.google == true &&
+    `
     padding: 3%;
     padding-left: 25px;
     `}
 `;
 
 export const MsgBox = styled.Text`
-    text-align: center;
-    font-size: 13px;
-    color: ${props => props.type == 'SUCCESS' ? green : red };
+  text-align: center;
+  font-size: 13px;
+  color: ${(props) => (props.type == "SUCCESS" ? green : red)};
 `;
 
 export const Line = styled.View`
-    height: 1px;
-    width: 100%;
-    background-color: ${blue};
-    margin-vertical: 10px;
+  height: 1px;
+  width: 100%;
+  background-color: ${blue};
+  margin-vertical: 10px;
 `;
 
 export const ExtraView = styled.View`
-    justify-content: center;
-    flex-direction: row;
-    align-items: center;
-    padding: 10px;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
 `;
 
 export const ExtraText = styled.Text`
-    justify-content: center;
-    align-content: center;
-    color: ${blue};
-    font-size: 15px;
+  justify-content: center;
+  align-content: center;
+  color: ${blue};
+  font-size: 15px;
 `;
 
 export const TextLink = styled.TouchableOpacity`
-    justify-content: center;
-    align-items: center;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const TextLinkContent = styled.Text`
-    color: ${purple};
-    font-size: 18px;
+  color: ${purple};
+  font-size: 18px;
 
-    ${(props) => {
-        const {resendStatus } = props;
-        if (resendStatus === 'Failed') {
-            return `color: ${red}`;
-        } else if (resendStatus === 'Sent!') {
-            return `color: ${green}`;
-        }
-    }}
+  ${(props) => {
+    const { resendStatus } = props;
+    if (resendStatus === "Failed") {
+      return `color: ${red}`;
+    } else if (resendStatus === "Sent!") {
+      return `color: ${green}`;
+    }
+  }}
 `;
 
 export const HomeContainer = styled(InnerContainer)`
-    padding: 25px;
-    padding-top: 10px;
-    justify-content: center;
-`
+  padding: 25px;
+  padding-top: 10px;
+  justify-content: center;
+`;
 
 export const Avatar = styled.Image`
-    width: 100px;
-    height: 100px;
-    margin: auto;
-    border-radius: 50px;
-    border-width: 2px;
-    border-color: ${pink};
-    margin-bottom: 10px;
-    margin-top: 10px;
-`
+  width: 100px;
+  height: 100px;
+  margin: auto;
+  border-radius: 50px;
+  border-width: 2px;
+  border-color: ${pink};
+  margin-bottom: 10px;
+  margin-top: 10px;
+`;
 
 export const HomeImage = styled.Image`
-    height: 30%;
-    min-width: 100%;
-`
+  height: 30%;
+  min-width: 100%;
+`;
 // verification components
 export const TopHalf = styled.View`
-    flex: 1;
-    justify-content: center;
-    align-content: center;
-    padding: 100px;
+  flex: 1;
+  justify-content: center;
+  align-content: center;
+  padding: 100px;
 `;
 
 export const BottomHalf = styled.View`
-    justify-content: space-around;
+  justify-content: space-around;
 `;
 
 export const IconBg = styled.View`
-    background-color: ${light_green};
-    border-radius: 250px;
-    height: 250px;
-    width: 250px;
-    justify-content: center; //centers icon vertically
-    align-items: center; //centers icon horizontally
-    margin-top: -60px; //moves IconBg up by half its height to center it vertically
-    display: flex;
-    justify-content: center;
+  background-color: ${light_green};
+  border-radius: 250px;
+  height: 250px;
+  width: 250px;
+  justify-content: center; //centers icon vertically
+  align-items: center; //centers icon horizontally
+  margin-top: -60px; //moves IconBg up by half its height to center it vertically
+  display: flex;
+  justify-content: center;
 `;
 
 export const InfoText = styled.Text`
-    color: ${grey};
-    font-size: 15px;
-    text-align: center;
+  color: ${grey};
+  font-size: 15px;
+  text-align: center;
 `;
 
 export const EmphasizeText = styled.Text`
-    font-weight: bold;
-    font-style: italic;
+  font-weight: bold;
+  font-style: italic;
 `;
 
 export const InlineGroup = styled.View`
-    flex-direction: 'row';
-    padding: 10px;
-    justify-content: center;
-    align-content: center;
+  flex-direction: "row";
+  padding: 10px;
+  justify-content: center;
+  align-content: center;
 `;
 
 export const AppBackground = styled.View`
@@ -284,13 +314,13 @@ export const AppBackground = styled.View`
 `;
 
 export const HomeStyledButton = styled.TouchableOpacity`
-    padding: 15px;
-    background-color: ${light_green};
-    justify-content: center;
-    align-items: center;
-    border-radius: 25px;
-    margin-vertical: 10px;
-    height: 60px;
+  padding: 15px;
+  background-color: ${light_green};
+  justify-content: center;
+  align-items: center;
+  border-radius: 25px;
+  margin-vertical: 10px;
+  height: 60px;
 `;
 
 export const ImageContainer = styled.View`
@@ -306,15 +336,13 @@ export const SelectedImage = styled.Image`
 `;
 
 export const CuteButton = styled.TouchableOpacity`
-    padding: 15px;
-    background-color: ${pink};
-    justify-content: center;
-    align-items: center;
-    border-radius: 25px;
-    margin-vertical: 10px;
-    margin-left: 20%;
-    margin-right: 20%;
-    height: 60px;
-`
-
-
+  padding: 15px;
+  background-color: ${pink};
+  justify-content: center;
+  align-items: center;
+  border-radius: 25px;
+  margin-vertical: 10px;
+  margin-left: 20%;
+  margin-right: 20%;
+  height: 60px;
+`;
